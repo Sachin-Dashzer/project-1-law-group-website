6cb7d4d2aefe8f9bfffd5b3d723537dadb8cbd39
Sachin Kumar <96949859+Sachin-Dashzer@users.noreply.github.com>
2024-08-07 11:00:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 
 <head>
-    <title>AMD | Home</title>
+    <title>AMDa | Home</title>
     <?php include 'include/head-links.php'; ?>
     <meta property="og:url" content="<?php echo $page_url ?>">
     <meta property="og:title" content="">
